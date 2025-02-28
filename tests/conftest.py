09eb383f4d90753a07a550714796534a1bce3cf5
olivia121033 <olivia.chung1210@gmail.com>
2025-02-28 16:54:54 +0000

    push1

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,14 @@
 # Placeholder
 from selenium.webdriver.chrome.options import Options
+import os
 
 def pytest_setup_options():
     options = Options()
+    if "GITHUB_ACTIONS" in os.environ:
+        options.add_argument("--headless")
+    else:
+        options.add_argument("start-maximized")
+
     #options.add_argument("--headless")  # Run Chrome in headless mode
     #service = Service(ChromeDriverManager().install())
     return options
